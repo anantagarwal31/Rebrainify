51e3a7e977cce18c4fd3861beb8cab7708149c8f
aa311003 <anantagarwal2003@gmail.com>
2025-07-11 19:34:59 +0530

    Button component created

--- Rebrainify-FE/src/App.tsx
+++ Rebrainify-FE/src/App.tsx
@@ -1,9 +1,12 @@
-import './App.css'
+import { Button } from "./components/Button"
+import { PlusIcon } from "./icons/PlusIcon"
+import { ShareIcon } from "./icons/ShareIcon"
+
 
 function App() {
-
   return <div>
-    hi there
+    <Button variant="primary" text="Add Content" startIcon={<PlusIcon/>}/>
+    <Button variant="secondary" text="Share Brain" startIcon={<ShareIcon/>}/>
   </div>
 }
 
