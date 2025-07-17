6c9da8de8328bc73f254efddf05c72e25f04b365
Anant Agarwal <anantagarwal2003@gmail.com>
2025-07-17 18:35:57 +0530

    created interfaces and fixed up errors till CreateContentModal Component

--- Rebrainify-FE/src/components/CreateContentModal.tsx
+++ Rebrainify-FE/src/components/CreateContentModal.tsx
@@ -2,7 +2,12 @@ import { CrossIcon } from "../icons/CrossIcon";
 import { Button } from "./Button";
 import { Input } from "./Input";
 
-export function CreateComponentModal({open, onClose}){
+interface CreateComponentModalProps{
+    open: Boolean,
+    onClose: ()=>void
+}
+
+export function CreateComponentModal({open, onClose}: CreateComponentModalProps){
     return <div>
         {open && (<div className="w-screen h-screen top-0 left-0  fixed flex justify-center items-center">
             <div className="absolute w-full h-full bg-slate-500 opacity-60"></div>
@@ -14,8 +19,8 @@ export function CreateComponentModal({open, onClose}){
                         </div>
                     </div>
                     <div>
-                        <Input placeholder={'Title'}/>
-                        <Input placeholder={'Link'}/>
+                        <Input placeholder={'Title'} onChange={()=>{}}/>
+                        <Input placeholder={'Link'} onChange={()=>{}}/>
                     </div>
                     <div className="flex justify-center pt-2">
                         <Button variant="primary" text="Submit"/>
--- Rebrainify-FE/src/components/Input.tsx
+++ Rebrainify-FE/src/components/Input.tsx
@@ -1,4 +1,9 @@
-export function Input({onChange, placeholder}:{onChange:()=>void}){
+interface InputProps{
+    placeholder: string,
+    onChange: ()=>void
+}
+
+export function Input({placeholder, onChange}:(InputProps)){
     return <div>
         <input placeholder={placeholder} type="text" className="px-4 py-2 border rounded m-2" onChange={onChange}></input>
     </div>
