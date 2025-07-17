d48f6b76468e19ac436d8128651090625024aa98
Anant Agarwal <anantagarwal2003@gmail.com>
2025-07-18 00:10:27 +0530

    Implemented handleClickOutside for CreateContentModal component

--- Rebrainify-FE/src/components/CreateContentModal.tsx
+++ Rebrainify-FE/src/components/CreateContentModal.tsx
@@ -1,3 +1,4 @@
+import { useEffect, useRef } from "react";
 import { CrossIcon } from "../icons/CrossIcon";
 import { Button } from "./Button";
 import { Input } from "./Input";
@@ -8,10 +9,26 @@ interface CreateComponentModalProps{
 }
 
 export function CreateComponentModal({open, onClose}: CreateComponentModalProps){
+    const modalRef = useRef<HTMLDivElement>(null);
+
+    useEffect(()=>{
+        function handleClickOutside(event:MouseEvent){
+            if(modalRef.current && !modalRef.current.contains(event.target as Node)){
+                onClose();
+            }
+        }
+        if(open){
+            document.addEventListener("mousedown",handleClickOutside)
+        }
+
+        return ()=>{
+            document.removeEventListener("mousedown", handleClickOutside)
+        };
+    },[open,onClose])
     return <div>
         {open && (<div className="w-screen h-screen top-0 left-0  fixed flex justify-center items-center">
             <div className="absolute w-full h-full bg-slate-500 opacity-60"></div>
-            <div className="z-10 flex flex-col justify-center">
+            <div ref={modalRef} className="z-10 flex flex-col justify-center">
                 <span className="bg-white p-4 rounded-md">
                     <div className="flex justify-end">
                         <div onClick={onClose} className="cursor-pointer">
