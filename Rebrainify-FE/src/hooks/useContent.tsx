6f0a64c59bdb78c9a1e1b8411101f5763e893cd8
Anant Agarwal <anantagarwal2003@gmail.com>
2025-07-25 18:39:59 +0530

    Share brain functionality implemented

--- Rebrainify-FE/src/hooks/useContent.tsx
+++ Rebrainify-FE/src/hooks/useContent.tsx
@@ -5,7 +5,7 @@ import axios from "axios"
 export function useContent(){
     const [contents, setContents] = useState([]);
 
-    useEffect(()=>{
+    function refresh(){
         axios.get(`${BACKEND_URL}/api/v1/content`,{
             headers:{
                 "Authorization": localStorage.getItem("token")
@@ -13,7 +13,18 @@ export function useContent(){
         }).then((response)=>{
             setContents(response.data.content);
         })
+    }
+
+    useEffect(()=>{
+        refresh();
+        let interval = setInterval(()=>{
+            refresh();
+        },5*1000)
+
+        return ()=>{
+            clearInterval(interval);
+        }
     },[])
 
-    return contents;
+    return {contents, refresh};
 }
--- Rebrainify-FE/src/pages/Dashboard.tsx
+++ Rebrainify-FE/src/pages/Dashboard.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react"
+import { useEffect, useState } from "react"
 import { Button } from "../components/Button"
 import { Card } from "../components/Card"
 import { CreateComponentModal } from "../components/CreateContentModal"
@@ -6,11 +6,17 @@ import { PlusIcon } from "../icons/PlusIcon"
 import { ShareIcon } from "../icons/ShareIcon"
 import { Sidebar } from "../components/Sidebar"
 import { useContent } from "../hooks/UseContent"
+import { BACKEND_URL } from "../config"
+import axios from "axios"
 
 
 export function Dashboard() {
   const [modalOpen, setModalOpen] = useState(false);
-  const contents = useContent();
+  const {contents, refresh} = useContent();
+
+  useEffect(()=>{
+    refresh();
+  },[modalOpen])
 
   return <div>
     <Sidebar/>
@@ -18,13 +24,23 @@ export function Dashboard() {
       <CreateComponentModal open={modalOpen} onClose={()=>{
         setModalOpen(false);
       }}/>
-      <div className="flex justify-end gap-4">
-        <Button variant="secondary" text="Share Brain" startIcon={<ShareIcon/>}/>
+      <div className="flex justify-end gap-4 ">
+        <Button onClick={async ()=>{
+          const response = await axios.post(`${BACKEND_URL}/api/v1/brain/share`,{
+            share: true
+          },{
+            headers:{
+              "Authorization": localStorage.getItem("token")
+            }
+          })
+          const shareURL = `http://localhost:5173/share/${response.data.hash}`
+          alert(shareURL);
+        }} variant="secondary" text="Share Brain" startIcon={<ShareIcon/>}/>
         <Button onClick={()=>{
           setModalOpen(true);
         }} variant="primary" text="Add Content" startIcon={<PlusIcon/>}/>
       </div>
-      <div className="flex gap-4">
+      <div className="flex gap-4 flex-wrap">
         {contents.map(({_id, title, link, type})=><Card key={_id} type={type} link={link} title={title}/>)}
       </div>
     </div>
